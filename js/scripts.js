c103afd62c42b42b651c1e46e230185b10583290
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-04-08 10:21:26 -0400

    reformatted pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,49 @@
 
 // pokemon height is in meters
 let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
-    { name: 'Ivysaur', height: 1, type: ['grass', 'poison'] },
-    { name: 'Venusaur', height: 2, type: ['grass', 'poison'] },
-    { name: 'Charmander', height: 0.6, type: ['fire'] },
-    { name: 'Charmeleon', height: 1.1, type: ['fire'] },
-    { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
-    { name: 'Squirtle', height: 0.5, type: ['water'] },
-    { name: 'Wartortle', height: 1, type: ['water'] },
-    { name: 'Blastoise', height: 1.6, type: ['water'] }
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        type: ['fire']
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1,
+        type: ['fire']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        type: ['fire', 'flying']
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        type: ['water']
+    },
+    {
+        name: 'Wartortle', 
+        height: 1,
+        type: ['water']
+    },
+    {
+        name: 'Blastoise',
+        height: 1.6,
+        type: ['water']
+    }
 ];
