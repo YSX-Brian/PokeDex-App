451e3dd1f4bbdc1a732894bf189364a68dbf6930
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-04-07 21:45:08 -0400

    created pokemonList array and added pokemon stats

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-alert('Hello World!');
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+// pokemon height is in meters
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+    { name: 'Ivysaur', height: 1, type: ['grass', 'poison'] },
+    { name: 'Venusaur', height: 2, type: ['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, type: ['fire'] },
+    { name: 'Charmeleon', height: 1.1, type: ['fire'] },
+    { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
+    { name: 'Squirtle', height: 0.5, type: ['water'] },
+    { name: 'Wartortle', height: 1, type: ['water'] },
+    { name: 'Blastoise', height: 1.6, type: ['water'] }
+];
